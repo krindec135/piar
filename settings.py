99549d2b94d65716bc6ff3b019446e1ce39a1217
krindec135 <70345464+krindec135@users.noreply.github.com>
2020-08-28 02:20:58 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+
+token = "1224625347:AAH7Dmh82d2XgIOVEnrKuNrA7Wh9IUHHpbM"
+
+admins = [1047585454] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 5 #минимум сабкоинов для раскрутки канала
+
+REF_BONUS = 2 #реферальный бонус (сабкоины)
+
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://graph.org/Vvedenie-v-bota-a-takzhe-pravila-05-11' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = '@sosibiburamil' #ссылка на чат
